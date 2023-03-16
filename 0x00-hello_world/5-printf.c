013ec8e67e9365ff528f0cbe63d859692b69ca1b
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-16 08:55:38 -0700

    bety butter

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-int main(void)
 /**
- * main  - entry point
- * return : always 0 ( success)
- */
+*main - Entry point
+*
+*Return : Always 0 (success)
+*/
+int main(void)
 {
-printf("Programming is like building a multilingual puzzle"\n);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
