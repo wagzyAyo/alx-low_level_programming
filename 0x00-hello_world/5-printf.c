98b0cde15b8caf2858e5eb180eb3ceb134f22bee
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-16 07:35:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n")
+#include<stdio.h>
+int main(void)
+{
+        printf("Programming is like building a multilingual puzzle"\n);
+        return (0);
+}
