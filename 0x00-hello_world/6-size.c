2676ae0585efc58f8baa720b4b2c6a77632dc5d5
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-16 09:10:19 -0700

    be6tty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-sizeof(char , int ,long int , long long int , float)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("size of a char:%d bytes(s)\n", sizeof(char));
+printf("size of a int:%d bytes(s)\n", sizeof(int));
+printf("size of a long int:%d bytes(s)\n", sizeof(long int));
+printf("size of a long long int:%d bytes(s)\n", sizeof(long long int));
+printf("size of a float:%d bytes(s)\n", sizeof(float));
+return (0);
+}
