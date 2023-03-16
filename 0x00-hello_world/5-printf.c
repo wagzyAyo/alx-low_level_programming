70212485dfd9a9256f436d9f0cf083c5eeee4054
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-16 08:07:27 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(void)
+/**
+ * main  - entry point
+ * return : always 0 ( success)
+ */
 {
-        printf("Programming is like building a multilingual puzzle"\n);
-        return (0);
+printf("Programming is like building a multilingual puzzle"\n);
+return (0);
 }
