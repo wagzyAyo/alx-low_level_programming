018122acd9e45fb1c916468968c81fd5a7c88dc4
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-23 06:57:52 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - checks for checks for a digit (0 through 9).
+* @a: a - Variable
+* @b: b - variable
+* Return: Always 0.
+*/
+int mult(int a, int b)
+{
+int mult;
+mult = a * b;
+return (mult);
+}
