2eec8582274846e2c44814b7c66abce4072907f8
wagzyAyo <olamidipupo1995@gmail.com>
2023-03-24 08:02:22 -0700

    review

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,16 @@ printf(" Fizz");
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} else if (i == 1)
+}
+else if (i == 1)
 {
 printf("%d", i);
-} else
+}
+else
 {
 printf(" %d", i);
 }
