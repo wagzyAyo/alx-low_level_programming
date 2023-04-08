98de2be88ef845d682150a4df1471fa60b04966d
wagzyAyo <olamidipupo1995@gmail.com>
2023-04-08 06:11:28 -0700

    000

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main: main function for command argc and argv
- * return : always 0 (success)
- */
+* main :  prints file name
+* return : always 0 (success)
+*/
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0)
+	return (0);
 }
