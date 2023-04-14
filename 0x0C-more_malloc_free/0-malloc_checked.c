61a1e3593757b8678160dd505151f690c05567e4
wagzyAyo <olamidipupo1995@gmail.com>
2023-04-14 01:05:47 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * iReturn: a pointer to memory allocated
- */
+* malloc_checked - allocates memory using malloc
+* @b: number of bytes to allocate
+*
+* Return: a pointer to memory allocated
+*/
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,14 @@
-#include <stdlib.h>		
-#include "main.h"		
+#include <stdlib.h>
+#include "main.h"
 /**
- * *_memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory area to be filled
- * @b: char to copy		
+ * @b: char to copy
  * @n: number of times to copy b
  *
  * Return: pointer to the memory area s
 */
-char *_memset(char *s, char b, unsigned int n)		
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int j;
 for (j = 0; j < n; j++)
@@ -16,15 +16,15 @@ for (j = 0; j < n; j++)
 s[j] = b;
 }
 return (s);
-}		
-/**		
- * *_calloc - allocates memory for an array		
- * @nmemb: number of elements in the array		
- * @size: size of each element		
- *		
- * Return: pointer to allocated memory		
+}
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size)		
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr;
 if (nmemb == 0 || size == 0)
