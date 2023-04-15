562db4f4d6010edb322330f64e5106e57c4fcb2a
wagzyAyo <olamidipupo1995@gmail.com>
2023-04-15 04:06:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>		
 #include "main.h"
 /**
-* _atoi - converts a string to an integer
-* @s: string to be converted
-*
-* Return: the int converted from the string
-*/
-int _atoi(char *s)
+* _atoi - converts a string to an integer		
+* @s: string to be converted		
+*		
+* Return: the int converted from the string		
+*/		
+int _atoi(char *s)		
 {
 int i, d, n, len, f, digit;
-i = 0;
+i = 0;		
 d = 0;
 n = 0;
 len = 0;
@@ -17,26 +17,26 @@ f = 0;
 digit = 0;
 while (s[len] != '\0')
 len++;
-while (i < len && f == 0)
+while (i < len && f == 0)		
 {
 if (s[i] == '-')
 ++d;
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
-if (d % 2)
+if(d % 2)
 digit = -digit;
 n = n * 10 + digit;
 f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
+if (s[i + 1] < '0' || s[i + 1] > '9')	
 break;
-f = 0;
+f = 0;	
 }
 i++;
 }
 if (f == 0)
 return (0);
-return (n);
+return (n);		
 }
 /**
 * main - multiplies two numbers
@@ -44,7 +44,7 @@ return (n);
 * @argv: array of arguments
 *
 * Return: 0 (Success), 1 (Error)
-*/
+*/		
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
@@ -57,5 +57,5 @@ num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
 result = num1 *num2;
 printf("%d\n", result);
-return (0);
+return (0);		
 }
