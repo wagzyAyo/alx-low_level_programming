04a788d91a433e4750299c512be3ff18bf806da2
wagzyAyo <talktojmcvibes@gmail.com>
2023-04-27 09:59:35 -0700

    fix ?

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++
+		s++;
 	}
-	return(s)
+	return(s);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -15,5 +15,5 @@ size_t list_len(const list_t *h)
 		i++;
 		h = h->next;
 	}
-	return(i)
+	return(i);
 }
