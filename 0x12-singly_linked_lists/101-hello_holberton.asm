08ec41b8290741671dadded98f128f1ff8b3ee05
wagzyAyo <talktojmcvibes@gmail.com>
2023-05-02 01:01:16 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
-section  .text
-   global main
-   extern printf
+global main
+extern printf
 main:
   mov edi , format
   xor eax, eax
-  mov ecx , msg
   call printf
   mov eax, 0
   ret
-format: db `Hello, Holberton`,10
+format: db `Hello, Holberton\n`,0
