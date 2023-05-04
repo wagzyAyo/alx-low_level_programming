521bd41140873d0ff53ee8348d07508da821bbbf
wagzyAyo <talktojmcvibes@gmail.com>
2023-05-04 07:26:22 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,10 +8,10 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int a;
-	unsigned int dec = 0;  //dec is decimal
+	unsigned int dec = 0;
 
 	if (!b)
-		return (0); //if there is no integer (b) return 0
+		return (0);
 
 	for (a = 0; b[a]; a++)
 	{
