78a1cd2337c7d90fd84456bf0543120656621e39
wagzyAyo <talktojmcvibes@gmail.com>
2023-05-04 07:55:16 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,13 +15,13 @@ void print_binary(unsigned long int n)
 
 		if (long_int & 1)
 		{
-			_putchar ('1');
+			_putchar('1');
 			numb++;
 		}
 		else if (numb)
-			_putchar ('0');
+			_putchar('0');
 	}
 
 	if (!numb)
-		_putchar ('0');
+		_putchar('0');
 }
